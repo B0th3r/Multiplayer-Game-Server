9ca8eb266ea170736f6525389032f7860b91c548
Bob Sea <KeshawnBbbryant@gmail.com>
2026-01-26 15:58:57 -0500

    feat(dialogue): register dialogue modules

---
+++ client/src/storymode/dialogue/index.js
@@ -0,0 +1,50 @@
+import jimDonna from "./jimDonna";
+import sam from "./sam";
+import john from "./john";
+import jane from "./jane";
+import bobbyBartender from "./bobbyBartender";
+import tim from "./tim";
+import hayes from "./hayes";
+import florist from "./florist";
+import lieutenant from "./lieutenant";
+import jackAlex from "./jackAlex";
+import bobbyMarcus from "./bobbyMarcus";
+import lucas from "./lucas";
+import lucasCity from "./lucasCity";
+import bobby from "./bobby";
+import gambler from "./gambler"
+import ace from "./ace"
+import maya from "./maya"
+import mayaBar from "./mayaBar"
+import marcus from "./marcus"
+import johnTim from "./johnTim"
+import marcusBar from "./marcusBar"
+import flower_promoter from "./flower_promoter"
+import delivery_girl from "./delivery_girl"
+
+export const DIALOGUE = {
+    jackAlex,
+    bobbyMarcus,
+    lucas,
+    hayes,
+    lieutenant,
+    florist,
+    jimDonna,
+    tim,
+    sam,
+    john,
+    jane,
+    bobby,
+    bobbyBartender,
+    lucasCity,
+    gambler,
+    ace,
+    maya,
+    mayaBar,
+    marcus,
+    johnTim,
+    marcusBar,
+    flower_promoter,
+    delivery_girl
+
+};
